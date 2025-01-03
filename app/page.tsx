bb002ea0bbc9e3ce194cb65cfb0d1722f8b91a65
Govind Vashishat <govindvashishat@gmail.com>
2025-01-03 20:54:05 +0530

    minor ui changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Image from "next/image"
 import Button1 from "./components/Button1"
 
 export default function LandingPage() {
-  return (
+  return (   
       <section className="max-w-7xl mx-auto flex flex-col items-center justify-center p-5">
         <div className="flex items-center justify-center text-white cursor-pointer border border-neutral-700 font-mono p-2 rounded-lg mt-2">Access to realistic avatars</div>
-        <h1 className="font-sans text-4xl text-center text-white mt-5">Create beautiful hyper realistic AI avatar videos with Avatrix</h1>
-        <p className="text-gray-400 p-2 font-sans">At Avatrix, brands can produce engaging, customizable video content featuring ultra-realistic avatars tailored to their identity. It empowers businesses to craft compelling marketing campaigns effortlessly, saving time and resources while boosting creativity and audience engagement.</p>
+        <h1 className="font-sans text-2xl md:text-4xl text-center text-white mt-5">Create beautiful hyper realistic AI avatar videos with Avatrix</h1>
+        <p className="text-gray-400 p-2 tezt-sm font-sans">At Avatrix, brands can produce engaging, customizable video content featuring ultra-realistic avatars tailored to their identity.</p>
         <Card className="shadow-2xl overflow-hidden border-none outline-none mt-6 mb-6">
           <Image 
             className="w-auto h-auto object-cover"
@@ -37,7 +37,7 @@ export default function LandingPage() {
           <div className="flex flex-col justify-center items-center px-4 mt-10">
             <h1 className="text-white text-center font-sans text-4xl">Get started</h1>
           <div className="md:w-[1000px] w-[400px] h-[200px] bg-zinc-900 rounded-2xl mt-5 font-sans ml-2 p-5 md:p-8">
-            <h1 className="text-center text-white">Simple, Straightforward and designed to help you market better.</h1>
+            <h1 className="text-center text-white">Simple, Straightforward and designed to help you make content faster.</h1>
             <p className="text-sm text-gray-400 mt-5 text-center">Ready to get started? Click on the button below to make your first AI avatar video!</p>
             <Button1 />
           </div>
